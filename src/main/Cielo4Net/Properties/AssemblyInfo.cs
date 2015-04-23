b6d51723e992eac2ae79a103eb5d4bcb3172e53b
Adriano Caldeira <adriano@tribecca.com.br>
2015-04-23 11:45:45 -0300

    documentação de assembly

--- src/main/Cielo4Net/Properties/AssemblyInfo.cs
+++ src/main/Cielo4Net/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cielo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Cielo4Net")]
+[assembly: AssemblyDescription("Biblioteca de integração com a solução WebService da Cielo. Maiores informações e detalhamento da solução da Cielo acesse o endereço https://www.cielo.com.br/ecommerce.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cielo")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("acaldeira.me")]
+[assembly: AssemblyProduct("Cielo4Net")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
